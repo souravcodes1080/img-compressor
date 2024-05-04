2c6710c99feb882ddb4c4300957c48c09ef1410d
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-05-04 19:51:24 +0530

    input fixed

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import right_arrow from "../../assets/right_arrow.png";
 import imageCompression from "browser-image-compression";
 
@@ -6,12 +6,14 @@ import "./homepage.css";
 function Homepage() {
   const [image, setImage] = useState(false);
   const [imageFile, setImageFile] = useState(null);
-  const [maxSizeMB, setMaxSizeMB] = useState("0.05")
-  const [maxResolution, setMaxResolution] = useState("1080")
+  const [maxSizeMB, setMaxSizeMB] = useState("250")
+  const [quality, setQuality] = useState("")
+  const [maxResolution, setMaxResolution] = useState("")
   const [imageName, setImageName] = useState("");
   const [compressedImage, setCompressedImage] = useState(null);
   const [compressedImageSize, setCompressedImageSize] = useState("");
   const [loading, setLoading] = useState(false);
+
 
   const onImageChangeHandler = (e) => {
     const imageFile = e.target.files[0];
@@ -23,9 +25,16 @@ function Homepage() {
   };
 
   const onSubmitHandler = async (e) => {
-    setLoading(true);
-    const options = {
-      maxSizeMB: maxSizeMB,
+    try {
+      if(!image){
+        return
+      }
+      if(maxResolution === ""){
+        return
+      }
+      setLoading(true);
+      const options = {
+      maxSizeMB: maxSizeMB /1024,
       useWebWorker: true,
       maxWidthOrHeight: maxResolution,
     };
@@ -33,7 +42,24 @@ function Homepage() {
     setCompressedImageSize(Math.floor(compressedFile.size / 1024));
     setCompressedImage(URL.createObjectURL(compressedFile));
     setLoading(false);
+    } catch (error) {
+      console.log(error)
+    }
+    
   };
+
+  useEffect(()=>{
+    if(maxSizeMB > 500){
+      setQuality("Best");
+    }
+    if(maxSizeMB <= 500){
+      setQuality("Better");
+    }
+    if(maxSizeMB <= 250){
+      setQuality("Good");
+    }
+    
+  }, [maxSizeMB])
 
   return (
     <>
@@ -51,14 +77,16 @@ function Homepage() {
                 accept="image/*"
                 onChange={onImageChangeHandler}
               />
+              
             </div>
             <div className="max-size-slider">
-              <p>Compression size</p>
-              <input type="range" min="0" max="1024" />
+              <p><span>Compression quality</span> {quality}</p> 
+              <input type="range" min="1" max="1024" value={maxSizeMB} onChange={(e)=>setMaxSizeMB(e.target.value)}/>
             </div>
 
             <select onChange={(e)=>setMaxResolution(e.target.value)} value={maxResolution}>
               <option value="">Select resolution</option>
+              <option value="1080">1080px</option>
               <option value="720">720px</option>
               <option value="480">480px</option>
               <option value="240">240px</option>
@@ -72,7 +100,7 @@ function Homepage() {
               </span>
               <span>{compressedImageSize} KB</span>
             </div>
-            <button onClick={onSubmitHandler}>
+            <button onClick={onSubmitHandler} className={!image ? 'disabled': '' }>
               <p>Compress Image</p>
               <img src={right_arrow} alt="right_arrow_icon" width={"15px"} />
             </button>
